6d7471c7775237081e9e5394be544f6efd63bbb3
zhixing <zhixing@shangyoo.com>
2017-03-03 18:42:59 +0800

    simple server

--- mustang/server.js
+++ mustang/server.js
@@ -2,7 +2,7 @@ var http = require('http')
   , port = 56789
 
 http.createServer(function(req, res) {
-    res.end('hello')
+    res.end('Hello, World!')
 }).listen(port)
 
 console.log('server is running on port', port)
