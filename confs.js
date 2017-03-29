c2151b21cbe2217f461cfb292b6f841d217ff22c
zhixing <zhixing@shangyoo.com>
2017-03-29 18:10:48 +0800

    static service

---
+++ confs.js
@@ -0,0 +1,3 @@
+exports.mustang = {
+    port : 56789
+}
--- mustang/server.js
+++ mustang/server.js
@@ -1,8 +1,16 @@
-var http = require('http')
-  , port = 56789
+var mod  = 'mustang'
+  , conf = require('../confs')[mod]
 
-http.createServer(function(req, res) {
-    res.end('Hello, World!')
-}).listen(port)
+var express = require('express')
+  , app = express()
 
-console.log('server is running on port', port)
+app.use(function(req, res, next) {
+    console.log('recv request')
+    next()
+})
+
+app.use(express.static('./public'))
+
+app.listen(conf.port, function() {
+    console.log('server is running on port', conf.port)
+})
