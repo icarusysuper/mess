b436ede4552ac87a587c5ccccb00312941e9ab53
zhixing <zhixing@shangyoo.com>
2017-03-29 18:37:08 +0800

    modify run.sh script

--- mustang/run.sh
+++ mustang/run.sh
@@ -5,12 +5,15 @@ sleep 1
 ulimit -n 99999
 sleep 1
 
-mv node.log $HOME/node_mustang_`date +%Y%m%d%H%M`.log
-touch node.log
+ADDR=`dirname $0`
+
+mv $ADDR/node.log $HOME/node_mustang_`date +%Y%m%d%H%M`.log
+touch $ADDR/node.log
 sleep 1
 
-file=`pwd`/server.js
-echo $file
-NODE_ENV=development node $file &>node.log&
+main=$ADDR/server.js
+log=$ADDR/node.log
+echo $main
+NODE_ENV=development node $main &>$log&
 
 ulimit -n
